03e28083cec2553bc397f7c89556b87290c6cbca
unknown <igpraise45@gmail.com>
2024-02-24 05:04:59 +0000

    created modifyArray elements prototype

---
+++ f5.cpp
@@ -0,0 +1,42 @@
+/*
+* FILE : f5.cpp
+* PROJECT : SENG1000-focuse 5
+* PROGRAMMER : ignatius chinonso
+* FIRST VERSION : 2024-02-23
+DESCRIPTION :Pass arrays as parameters.
+• Design computer programs and functions for modularity, maintainability, and ease of
+reuse
+*/
+
+
+
+
+
+#include <stdio.h>
+
+int getNum(void);
+void modifyArrayValues(int arrayOfDigits[], int size);
+
+
+int main(void)
+{
+    int arrayOfDigits[10] = {};
+
+   
+}
+
+#pragma warning(disable : 4996) // required by Visual Studio
+
+int getNum(void)
+{
+    char record[121] = {0}; /* record stores the string */
+    int number = 0;
+    if (sscanf(record, "%d", &number) != 1)
+    {
+        /* if the user did not enter a number recognizable by
+         * the system, set number to -1 */
+        number = -1;
+    }
+    return number;
+}
+
